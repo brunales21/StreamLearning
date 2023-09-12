c3213fa04e55912d5eceb3232671183aeb816ab7
brunales21 <bemimarket@gmail.com>
2023-09-12 18:46:14 +0200

    Class::method

---
+++ Clazz.java
@@ -0,0 +1,15 @@
+public class Clazz {
+    public String s;
+
+    public static boolean tieneVocal(String t) {
+        String v = "aeiou";
+        for (int i = 0; i < t.length(); i++) {
+            for (int j = 0; j < v.length(); j++) {
+                if (t.charAt(i) == v.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- Main.java
+++ Main.java
@@ -5,17 +5,21 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-        todosAlCuadrado(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10))
+        getPalabrasConVocal(Arrays.asList("hola", "zzz", "montiel", "bc"))
                 .forEach(System.out::println);
+
+
     }
-    //Filtrar una lista: Dada una lista de números, utiliza una expresión lambda para filtrar los números pares de la lista.
+    // 1 Filtrar una lista:
+    // Dada una lista de números, utiliza una expresión lambda para filtrar los números pares de la lista.
     public static List<Integer> getPares(List<Integer> numeros) {
+        Clazz c = new Clazz();
         return numeros.stream()
-                .filter(n -> n%2 == 0)
+                .filter(n -> n%2==0)
                 .collect(Collectors.toList());
     }
 
-    //Mapear una lista: Dada una lista de palabras, utiliza una expresión lambda para crear una nueva lista que contenga
+    // 2 Mapear una lista: Dada una lista de palabras, utiliza una expresión lambda para crear una nueva lista que contenga
     // la longitud de cada palabra en la lista original.
 
     public static List<Integer> getLens(List<String> palabras) {
@@ -27,25 +31,43 @@ public class Main {
         // .collect(Collectors.toList());
     }
 
-    //Reducir una lista: Dada una lista de números, utiliza una expresión lambda y la función reduce para calcular
+    // 3 Reducir una lista: Dada una lista de números, utiliza una expresión lambda y la función reduce para calcular
     // la suma de todos los números en la lista.
 
     public static int sumatorio(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (a, b) -> a+b);
-        // return numbers.stream().reduce(0, Integer::sum);
+        return numbers.stream()
+                .reduce(0, (a, b) -> a+b);
+        // return numbers.stream()
+        // .reduce(0, Integer::sum);
 
     }
 
-    //Ordenar una lista: Dada una lista de cadenas de texto, utiliza una expresión lambda y la
+    // 4 Ordenar una lista: Dada una lista de cadenas de texto, utiliza una expresión lambda y la
     // función sorted para ordenar las cadenas alfabéticamente.
 
     public static List<String> ordenar(List<String> palabras) {
-        return palabras.stream().sorted(String::compareTo).toList();
-        //return palabras.stream().sorted(a.compareTo(b)).collect(Collectors.toList());
+        return palabras.stream()
+                .sorted(String::compareTo)
+                .toList();
+        //return palabras.stream()
+        // .sorted((a, b) -> a.compareTo(b))
+        // .toList();
     }
 
-    //dada una lista calcula el cuadrado de todos los elementos
+    // 5 dada una lista calcula el cuadrado de todos los elementos
     public static List<Integer> todosAlCuadrado(List<Integer> numeros) {
-        return numeros.stream().map(n -> n*n).collect(Collectors.toList());
+        return numeros.stream()
+                .map(n -> n*n)
+                .toList();
+    }
+
+    // 6 Quitar las cadenas que no contengan vocal
+    public static List<String> getPalabrasConVocal(List<String> lista) {
+        return lista.stream().
+                filter(Clazz::tieneVocal).
+                collect(Collectors.toList());
+        //return lista.stream().
+        // filter(a -> Clazz.tieneVocal(a)).
+        // collect(Collectors.toList());
     }
 }
